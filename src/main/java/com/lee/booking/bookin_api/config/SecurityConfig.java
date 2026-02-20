eb604114c4e24bf8811b63e0cc92f244a6e6f088
Leander Van der Walt <lvdw4242@gmail.com>
2026-02-20 12:24:42 +0200

    Fix Path

--- src/main/java/com/lee/booking/bookin_api/config/SecurityConfig.java
+++ src/main/java/com/lee/booking/bookin_api/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+ import jakarta.servlet.http.HttpServletResponse;
+
 
 @Configuration
 public class SecurityConfig {
@@ -19,22 +21,27 @@ public class SecurityConfig {
         this.jwtAuthFilter = jwtAuthFilter;
     }
 
- @Bean
+
+@Bean
 public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth
-                    .requestMatchers(
-                            "/auth/**",
-                            "/swagger-ui/**",
-                            "/swagger-ui.html",
-                            "/v3/api-docs/**"
-                    ).permitAll()
-                    .anyRequest().authenticated()
-            )
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-            .build();
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .exceptionHandling(e -> e.authenticationEntryPoint((req, res, ex) ->
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+        ))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                "/", "/error",
+                "/auth/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html",
+                "/v3/api-docs", "/v3/api-docs/**"
+            ).permitAll()
+            .anyRequest().authenticated()
+        )
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        .build();
 }
 
     @Bean
