5dcae1dd3dfefe2d0ffdc6a5f9512340dc3e1e8c
Leander Van der Walt <lvdw4242@gmail.com>
2026-02-20 11:51:24 +0200

    Force HTTPS

--- src/main/java/com/lee/booking/bookin_api/config/OpenApiConfig.java
+++ src/main/java/com/lee/booking/bookin_api/config/OpenApiConfig.java
@@ -4,13 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
-        info = @Info(
-                title = "Booking API",
-                version = "v1",
-                description = "Appointment Booking Lite Backend"
-        )
+        info = @Info(title = "Booking API", version = "v1"),
+        servers = {
+                @Server(url = "https://booking-api-production-ba28.up.railway.app", description = "Production")
+        }
 )
 @SecurityScheme(
         name = "bearerAuth",
@@ -18,5 +18,4 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
         scheme = "bearer",
         bearerFormat = "JWT"
 )
-public class OpenApiConfig {
-}
+public class OpenApiConfig {}
